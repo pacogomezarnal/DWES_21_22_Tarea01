519515fff960f945fc4aa90b0ac92219d5f4351a
pacogomezarnal <paco.gomez.arnal@gmail.com>
2021-10-16 19:36:06 +0200

    Proyecto01

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto01</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+<body>
+    <h2>Página principal</h2>
+    <div>
+        <p><?php echo "Bienvenido a la página principal"?></p>
+        <ul>
+            <li><?php print("<a href='producto.php'>Ir a la página de producto</a>")?></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ producto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto01</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+<body>
+    <h2>Página producto</h2>
+    <div>
+        <p><?php echo "Bienvenido a la página de producto"?></p>
+        <div>
+            <img src="./img/producto.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
